7c4caed0fc01ebee3f19988e0c3d219dcf57acff
char-tan <hello.spam@tutanota.com>
2022-12-17 17:36:57 +0000

    make mlp models same as paper

--- models/mlp.py
+++ models/mlp.py
@@ -7,8 +7,7 @@ class MLP(nn.Module):
     self.layer0 = nn.Linear(input, 512)
     self.layer1 = nn.Linear(512, 512)
     self.layer2 = nn.Linear(512, 512)
-    self.layer3 = nn.Linear(512, 512)
-    self.layer4 = nn.Linear(512, 10)
+    self.layer3 = nn.Linear(512, 10
 
 
   def forward(self, x):
@@ -16,7 +15,6 @@ class MLP(nn.Module):
     x = nn.functional.relu(self.layer0(x))
     x = nn.functional.relu(self.layer1(x))
     x = nn.functional.relu(self.layer2(x))
-    x = nn.functional.relu(self.layer3(x))
-    x = self.layer4(x)
+    x = self.layer3(x)
 
     return nn.functional.log_softmax(x)
