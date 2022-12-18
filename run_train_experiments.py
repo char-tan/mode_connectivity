f1d2cc567b0bd9cf2006296c7e0cf77c3ba8b094
char-tan <hello.spam@tutanota.com>
2022-12-18 00:52:47 +0000

    save models after training

--- run_train_experiments.py
+++ run_train_experiments.py
@@ -1,3 +1,4 @@
+import torch
 from train_experiments import *
 
 experiment_config = MLP_MNIST_DEFAULT
@@ -7,7 +8,11 @@ experiment_config.seed = 7
 model_a,device,train_loader,test_loader,optimizer,epochs,scheduler,log_interval = setup_train(experiment_config)
 run_training(model_a,device,train_loader,test_loader,optimizer,epochs,scheduler,log_interval, verbose=1)
 
+torch.save(model_a.state_dict(), 'model_a.pt')
+
 # Change seed to get different model
 experiment_config.seed = 42
 model_b,device,train_loader,test_loader,optimizer,epochs,scheduler,log_interval = setup_train(experiment_config)
 run_training(model_b,device,train_loader,test_loader,optimizer,epochs,scheduler,log_interval, verbose=1)
+
+torch.save(model_b.state_dict(), 'model_a.pt')
