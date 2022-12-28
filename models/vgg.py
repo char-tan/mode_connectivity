470c601aa5cd270cebfdfca01ecb37f05a1d0899
char-tan <hello.spam@tutanota.com>
2022-12-28 14:24:39 +0000

    rm bias from vgg convs, tidy vgg permutation_spec

--- models/vgg.py
+++ models/vgg.py
@@ -44,7 +44,7 @@ class VGG(nn.Module):
 
                 # add the conv layer to layers
                 layers += [
-                        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
                         nn.LayerNorm([out_channels, spatial_dim, spatial_dim]),
                         nn.ReLU(),
                         ]
@@ -68,35 +68,37 @@ class VGG(nn.Module):
         return nn.Sequential(*layers)
 
     def _permutation_spec(self):
-        layers_with_conv = [3,7,10,14,17,20,24,27,30,34,37,40]
-        layers_with_conv_b4 = [0,3,7,10,14,17,20,24,27,30,34,37]
-        layers_with_bn = [4,8,11,15,18,21,25,28,31,35,38,41]
-        dense = lambda name, p_in, p_out, bias = True: {f"{name}.weight": (p_out, p_in), f"{name}.bias": (p_out, )}
-
-        num_hidden_layers = 3
+        
+        # layer descriptions
+        is_conv = [3,7,10,14,17,20,24,27,30,34,37,40]
+        follows_conv = [0,3,7,10,14,17,20,24,27,30,34,37]
+        is_norm = [4,8,11,15,18,21,25,28,31,35,38,41]
 
         return permutation_spec_from_axes_to_perm({
-            # first features
+
+            # first conv
             "features.0.weight": ( "P_Conv_0",None, None, None),
+
+            # first norm
             "features.1.weight": ( "P_Conv_0", None),
             "features.1.bias": ( "P_Conv_0", None),
 
             # conv layers
-            **{f"features.{layers_with_conv[i]}.weight": ( f"P_Conv_{layers_with_conv[i]}", f"P_Conv_{layers_with_conv_b4[i]}", None, None, )
-                for i in range(len(layers_with_conv))},
+            **{f"features.{is_conv[i]}.weight": ( f"P_Conv_{is_conv[i]}", f"P_Conv_{follows_conv[i]}", None, None, )
+                for i in range(len(is_conv))},
             **{f"features.{i}.bias": (f"P_Conv_{i}", )
-                for i in layers_with_conv + [0]},
+                for i in is_conv + [0]},
 
-            # batch norm
-            **{f"features.{layers_with_bn[i]}.weight": ( f"P_Conv_{layers_with_conv[i]}", None)
-                for i in range(len(layers_with_bn))},
-            **{f"features.{layers_with_bn[i]}.bias": ( f"P_Conv_{layers_with_conv[i]}", None)
-                for i in range(len(layers_with_bn))},
+            # layer norms
+            **{f"features.{is_norm[i]}.weight": ( f"P_Conv_{is_conv[i]}", None)
+                for i in range(len(is_norm))},
+            **{f"features.{is_norm[i]}.bias": ( f"P_Conv_{is_conv[i]}", None)
+                for i in range(len(is_norm))},
 
             # classifier
-            "classifier.0.weight": ("P_Dense_0", f"P_Conv_{layers_with_conv[-1]}"),
+            "classifier.0.weight": ("P_Dense_0", f"P_Conv_{is_conv[-1]}"),
             "classifier.2.weight": ( f"P_Dense_2", f"P_Dense_0"),
             "classifier.2.bias": ("P_Dense_2", ),
-            "classifier.4.weight": (None, "P_Dense_4}"),
+            "classifier.4.weight": (None, "P_Dense_4"),
             "classifier.4.bias": (None, ),
             })
