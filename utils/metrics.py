7ee10c522bf1e6d1d53fb49fd4593f6479a59800
char-tan <hello.spam@tutanota.com>
2023-01-13 16:07:47 +0000

    rm sq euc dist from utils.metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,6 @@
 # %%
 import torch
 import torch.nn.functional as f
-from utils.utils import state_dict_to_torch_tensor
 
 def kl_div(logP, M):
     # kl_div over probability distributions
@@ -12,11 +11,4 @@ def JSD_loss(input_a, input_b):
     M = (input_a.softmax(-1)+input_b.softmax(-1)) / 2
     # note that f.kl_div takes log-probs
     return (kl_div(input_a, M) + kl_div(input_b, M)) / 2
-
-def squared_euclid_dist(model_a, model_b, batch_imgs):
-    a_params = model_a.state_dict()
-    b_params = model_b.state_dict()
-    a_vect = state_dict_to_torch_tensor(a_params) 
-    b_vect = state_dict_to_torch_tensor(b_params)
-    return ((a_vect - b_vect)**2).sum()
 
