7b4927d5463a7439d08e84d001035fe1d2d2085e
char-tan <hello.spam@tutanota.com>
2023-01-13 21:34:22 +0000

    JSD fixed

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,12 +4,15 @@ import torch.nn.functional as F
 
 def JSD_loss(logits_P, logits_Q):
 
-    P = torch.softmax(logits_P)
-    Q = torch.softmax(logits_Q)
+    P = torch.softmax(logits_P, dim=-1)
+    Q = torch.softmax(logits_Q, dim=-1)
 
     M = (P + Q) / 2
 
-    logP, logQ, logM = torch.log(logP), torch.log(logQ), torch.log(logM)
+    logP, logQ, logM = torch.log(P), torch.log(Q), torch.log(M)
 
     # PT KL Div is reversed input
-    return (F.kl_div(logM, logP) + F.kl_div(logM, logQ)) / 2
+    JSD = (F.kl_div(logM, logP, log_target=True, reduction='batchmean') + 
+                  F.kl_div(logM, logQ, log_target=True, reduction='batchmean')) / 2
+    
+    return JSD
